4d8098fdaecaecf2b43c98841309fbcc774ec516
asude <asudeguven48@gmail.com>
2025-07-08 14:12:17 +0300

    @Repository eklendi

--- src/main/java/com/example/InternshipProject/repositories/AssignmentRepository.java
+++ src/main/java/com/example/InternshipProject/repositories/AssignmentRepository.java
@@ -1,8 +1,9 @@
 package com.example.InternshipProject.repositories;
 import com.example.InternshipProject.entities.concretes.Assignment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-
+@Repository
 public interface AssignmentRepository extends JpaRepository<Assignment, Integer>{
 
 }
