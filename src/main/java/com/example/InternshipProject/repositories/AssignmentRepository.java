27a59f54e29f4e9ec85142d0923c3f62e00fbf56
asude <asudeguven48@gmail.com>
2025-07-08 14:08:26 +0300

    AssignmentRepository.java kısmı eklendi

---
+++ src/main/java/com/example/InternshipProject/repositories/AssignmentRepository.java
@@ -0,0 +1,8 @@
+package com.example.InternshipProject.repositories;
+import com.example.InternshipProject.entities.concretes.Assignment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface AssignmentRepository extends JpaRepository<Assignment, Integer>{
+
+}
