b2ffd56737bf786d78ec9ef23882558e9b941d99
asude <asudeguven48@gmail.com>
2025-07-11 16:20:07 +0300

    added @CrossOrigin to mentor and assignment controller

--- src/main/java/com/example/InternshipProject/controllers/AssignmentController.java
+++ src/main/java/com/example/InternshipProject/controllers/AssignmentController.java
@@ -12,6 +12,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/assignments")
 @RequiredArgsConstructor
+@CrossOrigin
 public class AssignmentController {
 
     private final AssignmentService assignmentService;
--- src/main/java/com/example/InternshipProject/controllers/MentorController.java
+++ src/main/java/com/example/InternshipProject/controllers/MentorController.java
@@ -9,11 +9,17 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/mentors")
+@CrossOrigin
 public class MentorController {
     private final MentorService mentorService;
     @Autowired
     public MentorController (MentorService mentorService){
         this.mentorService=mentorService;
+    }
+    @GetMapping("/api/greeting")
+    public String getGreeting() {
+        // This is the data you are sending to the frontend
+        return "{\"message\": \"Hello from your Java Backend!\"}";
     }
     @GetMapping("/{id}")
     public Mentor getMentorById(@PathVariable int id){return mentorService.getMentorById(id);}
