54822c57a72e167aa8ff1d2a0e8f883255c23ff7
yigitemre2003 <yigitemre2003@gmail.com>
2025-07-18 11:21:08 +0300

    Added Intern and Mentor responses

---
+++ src/main/java/com/example/InternshipProject/services/dtos/responses/MentorResponse.java
@@ -0,0 +1,15 @@
+package com.example.InternshipProject.services.dtos.responses;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class MentorResponse {
+    private String name;
+    private String surname;
+    private String email;
+    private String department;
+}
